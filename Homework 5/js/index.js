6165261e8480e5e716879492778c6da8048e96ff
Elian Kurtev <lqnko_55@outlook.com>
2018-03-06 19:03:12 +0200

    added new functions and img

--- Homework 5/js/index.js
+++ Homework 5/js/index.js
@@ -1,32 +1,67 @@
 $(document).ready(function() {
-    var url = 'https://www.bitstamp.net/api/v2/ticker_hour/btceur/';
-    setInterval(function() {
-
-    }, 3000);
-    $.get(url, function(data, status) {
-
-        console.log('Status:', status);
-        console.log('data:', data);
-        var fixBNB = 1.95583;
-
-        var btc = data.last;
-        var btcInBGN = btc * fixBNB;
-        console.log(Math.round(btcInBGN * 100) / 100);
-        var section = document.querySelector('#template');
-        var myArticle = document.createElement('article');
-        var H1 = document.createElement('h1');
-        myArticle.appendChild(H1);
-
-        H1.textContent = Math.round(btcInBGN * 100) / 100 + " лв."; //rounded to the second dec
-        section.appendChild(myArticle);
-        /*setInterval(function() {
-          btcInBGN.
-            //$('h1').load(url)
-        }, 2000);*/
 
 
+    /*FUNCTION THAT SHOWS THE INFORMATION IN THE INDEX.HTML FILE ABOUT BTCS RATE*/
+    function print(stoinost, imgSrc) {
+        $("#template p").text("1 BTC = " + valueFinal);
 
-    }); //function closing
+        $("#template img").attr("src", imgSrc);
+    } /*CLOSING "print" FUNCTION */
 
+    /*FUNCTION THAT LOADS FROM EXTERNAL JSON FILE THE INFO ABOUT THE RATE OF BTC IN BGN FOR FIRST TIME. 
+        15 SECS AFTER THE "function reloadSecondURL" LOADS*/
+    var valueFinal = 0.;
 
-}); //on document ready
+    function reload() {
+        $.get('https://www.bitstamp.net/api/v2/ticker_hour/btceur/', function(data, status) {
+            var fixBNB = 1.95583; /*EUR IN BGN - INFO FROM BNB*/
+            console.log('Status:', status);
+            console.log('data:', data);
+            var btc = data.last; /*GETTING THE LAST VALUE FOR 1BTC*/
+            var btcTMP = btc; /* TRANSFERING THE VALUE OF 1BTC TO TEMPORARY VARIABLE TO MAKE COMAPRISOON BETWEEN THEM*/
+            if (btcTMP != btc) { /*DOING A COMAPRISON BETWEEN THE 2 VARIABLES AND TAKE THE CORRESPONDING RESULT*/
+                //console.log(btcTMP);
+                var btcInBGN = btcTMP * fixBNB;
+                //console.log(btcInBGN);
+            } else {
+                //console.log(btc);
+                var btcInBGN = btc * fixBNB;
+                //console.log(btcInBGN);
+            }
+
+            valueFinal = ((btcInBGN).toFixed(2)) + " лв.";
+            //console.log(valueFinal);
+            var srcIMG = "../images/arrow-up.png";
+            print(valueFinal, srcIMG);
+
+        });
+    }; /*CLOSING "reload" function*/
+    reload();
+
+    /*FUNCTION THAT SHOWS THE LAST RATE INFO DOR BTC ON EVERY 15 SECS*/
+    function reloadSecondURL() {
+        $.get('https://www.bitstamp.net/api/v2/ticker_hour/btceur/', function(data, status) {
+            var fixBNB = 1.95583; /*EUR IN BGN - INFO FROM BNB*/
+            console.log('Status:', status);
+            console.log('data:', data);
+            var btc = data.last; /*GETTING THE LAST VALUE FOR 1BTC*/
+            var btcTMP = btc; /* TRANSFERING THE VALUE OF 1BTC TO TEMPORARY VARIABLE TO MAKE COMAPRISOON BETWEEN THEM*/
+            if (btcTMP != btc) { /*DOING A COMAPRISON BETWEEN THE 2 VARIABLES AND TAKE THE CORRESPONDING RESULT*/
+                //console.log(btcTMP);
+                var btcInBGN = btcTMP * fixBNB;
+                //console.log(btcInBGN);
+            } else {
+                //console.log(btc);
+                var btcInBGN = btc * fixBNB;
+                //console.log(btcInBGN);
+            }
+
+            valueFinal = ((btcInBGN).toFixed(2)) + " лв.";
+            //console.log(valueFinal);
+            print(valueFinal);
+
+        });
+    };
+
+    setInterval(reloadSecondURL, 15000); //RELOADING ON EVERY 15 SEC THE FUNCTION WITH UPDATED DATA 
+}); //ON DOCUMENT READY
